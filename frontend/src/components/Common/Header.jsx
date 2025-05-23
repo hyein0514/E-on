4d559bab47e689ab1fb8f018648a54b288969857
ownue <enu_i@naver.com>
2025-05-23 12:53:35 +0900

    feat(front): Header 제작 중
    - 로고 클릭 시 Home으로 리다이렉션
    - 간단한 css 수정 중

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,19 @@
+// Header.jsx
+
+import styles from "./../../styles/Common/Header.module.css";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import logo from "./../../../public/logo.svg";
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.logoContainer}>
+                <Link to="/" className={styles.logoLink}>
+                    <img src={logo} alt="Logo" className={styles.logo} />
+                </Link>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- frontend/src/pages/Calendar/Calendar.jsx
+++ frontend/src/pages/Calendar/Calendar.jsx
@@ -1,8 +1,14 @@
-// Calendar.Calendar.jsx
+// Calendar/Calendar.jsx
+
+import styles from "../../styles/Pages/Calendar.module.css";
+import Header from "../../components/Common/Header";
 
 const Calendar = () => {
     return (
         <div>
+            <div className={styles.header}>
+                <Header />
+            </div>
             <h1>Calendar Page</h1>
         </div>
     );

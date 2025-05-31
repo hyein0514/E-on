f2d41218b76d9be791a67610b6b27feed723d7fa
hyen <hi020514@gmail.com>
2025-05-31 12:00:22 +0900

    fix

--- frontend/src/router/AppRoutes.jsx
+++ frontend/src/router/AppRoutes.jsx
@@ -7,10 +7,13 @@ import Suggestion from "../pages/Suggestion/Suggestion";
 import CommunityList from "../pages/Community/CommunityList";
 import MyPage from "../pages/MyPage/MyPage";
 import PrivateRoutes from "./PrivateRoutes";
-<<<<<<< HEAD
 import ChallengeCreate from "../pages/Challenge/ChallengeCreate";
-=======
->>>>>>> 40933ce (main 브랜치에서 frontend 폴더만 가져옴)
+import ChallengeDetail from "../pages/Challenge/ChallengeDetail";
+import Attendance from "../pages/Challenge/Attendance";
+import ReviewList from "../pages/Challenge/ReviewList";
+import ReviewCreate from "../pages/Challenge/ReviewCreate";
+import ChallengeEdit from "../pages/Challenge/ChallengeEdit";
+import ReviewEdit from "../pages/Challenge/ReviewEdit";
 
 const AppRoutes = () => {
     return (
@@ -19,26 +22,23 @@ const AppRoutes = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/challenge" element={<Challenge />} />
-<<<<<<< HEAD
             <Route path="/challenge/create" element={<ChallengeCreate />} />
             <Route path="/suggestion" element={<Suggestion />} />
             <Route path="/community" element={<CommunityList />} />
+            <Route path="/attendance/:challengeId" element={<Attendance />} />
+            <Route path="/challenge/:challengeId/reviews" element={<ReviewList />} />
+            <Route path="/challenge/:challengeId/review/create" element={<ReviewCreate />} />
+            <Route path="/challenge/:id" element={<ChallengeDetail />} />
+            <Route path="/challenge/:id/edit" element={<ChallengeEdit />} />
+            <Route path="/challenge/:challengeId/review/:reviewId/edit" element={<ReviewEdit />} />
             {/*<Route
-=======
-            <Route path="/suggestion" element={<Suggestion />} />
-            <Route
->>>>>>> 40933ce (main 브랜치에서 frontend 폴더만 가져옴)
                 path="/community"
                 element={
                     <PrivateRoutes>
                         <CommunityList />
                     </PrivateRoutes>
                 }
-<<<<<<< HEAD
             />*/}
-=======
-            />
->>>>>>> 40933ce (main 브랜치에서 frontend 폴더만 가져옴)
             <Route
                 path="/mypage/:userId"
                 element={

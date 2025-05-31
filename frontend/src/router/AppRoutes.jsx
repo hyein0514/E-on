1eed0c4b15096006a73fc01b242496057ebc4a5b
hyen <hi020514@gmail.com>
2025-05-31 12:00:22 +0900

    fix

--- frontend/src/router/AppRoutes.jsx
+++ frontend/src/router/AppRoutes.jsx
@@ -7,10 +7,7 @@ import Suggestion from "../pages/Suggestion/Suggestion";
 import CommunityList from "../pages/Community/CommunityList";
 import MyPage from "../pages/MyPage/MyPage";
 import PrivateRoutes from "./PrivateRoutes";
-<<<<<<< HEAD
-=======
 import ChallengeCreate from "../pages/Challenge/ChallengeCreate";
->>>>>>> 63c6133e9bbb9d9084d893adc3834550947d4d39
 
 const AppRoutes = () => {
     return (
@@ -19,26 +16,17 @@ const AppRoutes = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/challenge" element={<Challenge />} />
-<<<<<<< HEAD
-            <Route path="/suggestion" element={<Suggestion />} />
-            <Route
-=======
             <Route path="/challenge/create" element={<ChallengeCreate />} />
             <Route path="/suggestion" element={<Suggestion />} />
             <Route path="/community" element={<CommunityList />} />
             {/*<Route
->>>>>>> 63c6133e9bbb9d9084d893adc3834550947d4d39
                 path="/community"
                 element={
                     <PrivateRoutes>
                         <CommunityList />
                     </PrivateRoutes>
                 }
-<<<<<<< HEAD
-            />
-=======
             />*/}
->>>>>>> 63c6133e9bbb9d9084d893adc3834550947d4d39
             <Route
                 path="/mypage/:userId"
                 element={

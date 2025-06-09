793fa76b51be07e61758ffafdd936bec88baa728
romdy <kimjh0937@gmail.com>
2025-06-09 23:21:50 +0900

    fix: 챌린지 참여 신청 및 취소 api 에러 수정

--- backend/routes/participationRoutes.js
+++ backend/routes/participationRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/participationController');
 
-router.post('/challenges/:id/participations', ctrl.join);
-router.patch('/participations/:id', ctrl.cancel);
-router.get('/participations/:id', ctrl.getOne); // 챌린지 조회
+router.post('/challenges/:id', ctrl.join);
+router.patch('/:id', ctrl.cancel);
+router.get('/:id', ctrl.getOne); // 챌린지 조회
 
 router.get('/challenge/:challengeId/user/:userId', ctrl.getParticipationByUserAndChallenge);
 

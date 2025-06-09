acad13ba5f4fd605a40f74050eccf2f97aa925ce
hyen <hi020514@gmail.com>
2025-06-09 23:47:54 +0900

    fix:api수정

--- backend/routes/attachmentRoutes.js
+++ backend/routes/attachmentRoutes.js
@@ -3,16 +3,7 @@ const router  = express.Router();
 const upload = require('../config/multer');
 const ctrl    = require('../controllers/attachmentController');
 
-// router.post('/challenges/:id/attachments',upload.array('files'), ctrl.add);
-router.post(
-  '/challenges/:id/attachments',
-  upload.fields([
-    { name: 'photos',   maxCount: 5 },
-    { name: 'consents', maxCount: 1 }
-  ]),
-  ctrl.add
-);
-router.get('/challenges/:id/attachments',ctrl.list);
-router.delete('/attachments/:id',ctrl.remove);
+
+router.delete('/:id',ctrl.remove);
 
 module.exports = router;

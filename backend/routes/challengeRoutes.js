791f813947d27859ee862e2997eb26b3ea082233
romdy <kimjh0937@gmail.com>
2025-06-09 22:53:16 +0900

    fix: 챌린지 api 에러 수정

--- backend/routes/challengeRoutes.js
+++ backend/routes/challengeRoutes.js
@@ -7,15 +7,15 @@ const ctrl    = require('../controllers/challengeController');
 const router = express.Router();
 
 router.post(
-    '/challenges',
+    '/',
     upload.array('files'),            
     ctrl.create
   );
 
-router.get('/challenges', ctrl.list); // 챌린지 조회
-router.get('/challenges/:id', ctrl.detail); // 챌린지 상세 조회
-router.patch('/challenges/:id', ctrl.update); //챌린지 수정
-router.delete('/challenges/:id', ctrl.remove); 
-router.patch('/challenges/:id/state', ctrl.changeState); //상태변경
+router.get('/', ctrl.list); // 챌린지 조회
+router.get('/:id', ctrl.detail); // 챌린지 상세 조회
+router.patch('/:id', ctrl.update); //챌린지 수정
+router.delete('/:id', ctrl.remove); 
+router.patch('/:id/state', ctrl.changeState); //상태변경
 
 module.exports = router;

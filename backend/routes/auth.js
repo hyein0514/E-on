734766f46afdd01e78b7d5a3bcb5b240b955d6b1
thatsgoodd <dltndus5985@gmail.com>
2025-06-18 09:44:43 +0900

    비활성화 계정 부분 수정

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const authCtrl = require('../controllers/auth');
-const { isNotLoggedIn } = require('../middleware/auth');
+const { isLoggedIn, isNotLoggedIn } = require('../middleware/auth');
 
-router.post('/join/step1',    isNotLoggedIn, authCtrl.signupStep1);
-router.post('/join/step2',    isNotLoggedIn, authCtrl.signupStep2);
-router.post('/join/email',    isNotLoggedIn, authCtrl.sendEmailCode);
-router.post('/verify-email',  isNotLoggedIn, authCtrl.verifyEmailCode);
-router.post('/join/step3',    isNotLoggedIn, authCtrl.signupStep3);
+// 회원가입
+router.post('/join/step1', isNotLoggedIn, authCtrl.signupStep1);
+router.post('/join/step2', isNotLoggedIn, authCtrl.signupStep2);
+router.post('/join/email', isNotLoggedIn, authCtrl.sendEmailCode);
+router.post('/verify-email', isNotLoggedIn, authCtrl.verifyEmailCode);
+router.post('/join/step3', isNotLoggedIn, authCtrl.signupStep3);
 
-// 로그인
-router.post('/login', authCtrl.login);
-
-// 로그아웃
-router.post('/logout', authCtrl.logout);
-
-// router.get("/refresh", authCtrl.refresh)
+// 로그인 / 로그아웃
+router.post('/login', isNotLoggedIn, authCtrl.login);   // 로그인은 로그인 안 한 사람만
+router.post('/logout', isLoggedIn, authCtrl.logout);    // 로그아웃은 로그인한 사람만
 
 module.exports = router;
